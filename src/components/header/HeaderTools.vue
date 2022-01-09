3e32f139b63aecd56e3ce7fd42393d5d86be5e9a
Zhao KK <kikakkz@hotmail.com>
2022-01-09 15:47:07 +0800

    Add github click

--- src/components/header/HeaderTools.vue
+++ src/components/header/HeaderTools.vue
@@ -1,15 +1,19 @@
 <template>
   <div class='row'>
     <HeaderToolBtn icon='icons/github.png' @click='onGithubClick' />
-    <Avatar />
+    <Avatar v-if='logined' />
   </div>
 </template>
 
 <script setup lang='ts'>
-import { defineAsyncComponent } from 'vue'
+import { defineAsyncComponent, computed } from 'vue'
+import { useStore } from '../../store'
 
 const Avatar = defineAsyncComponent(() => import('src/components/avatar/Avatar.vue'))
 const HeaderToolBtn = defineAsyncComponent(() => import('src/components/header/HeaderToolBtn.vue'))
+
+const store = useStore()
+const logined = computed(() => store.getters.getLogined)
 
 const onGithubClick = () => {
   window.open('https://github.com')
