569460f08fc8d7ed7e18f5b1d16ffd53a550104d
Zhao KK <kikakkz@hotmail.com>
2022-01-14 13:13:06 +0800

    Use v2

--- src/store/goods/const.ts
+++ src/store/goods/const.ts
@@ -1,5 +1,5 @@
 enum API {
-  GET_ALL_GOODS = '/cloud-hashing-apis/v1/get/goods/detail',
+  GET_ALL_GOODS = '/cloud-hashing-apis-v2/v1/get/goods',
   GET_ALL_DEVICES = '/cloud-hashing-goods/v1/get/devices',
   GET_ALL_VENDOR_LOCATIONS = '/cloud-hashing-goods/v1/get/vendor-locations',
   CREATE_DEVICE = '/cloud-hashing-goods/v1/create/device',
--- src/store/reviews/const.ts
+++ src/store/reviews/const.ts
@@ -1,6 +1,6 @@
 enum API {
-  GET_KYC_REVIEWS = '/cloud-hashing-apis/v1/get/kyc/reviews',
-  GET_GOOD_REVIEWS = '/cloud-hashing-apis/v1/get/good/reviews'
+  GET_KYC_REVIEWS = '/cloud-hashing-apis-v2/v1/get/kyc/reviews',
+  GET_GOOD_REVIEWS = '/cloud-hashing-apis-v2/v1/get/good/reviews'
 }
 
 export {
